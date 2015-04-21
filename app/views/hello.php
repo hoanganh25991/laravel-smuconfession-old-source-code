9dcd36bbed049029eb09902d3134ea6c559b66f9
Robert Sim <robert.sim.2010@sis.smu.edu.sg>
2015-04-22 01:43:19 +0800

    no blade for landing

--- app/views/hello.php
+++ app/views/hello.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap/3.3.4/css/bootstrap.min.css">
 
     <!-- Custom CSS -->
-    <link href="{{ url(css/full-width-pics.css)}}" rel="stylesheet">
+    <link href="css/full-width-pics.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
